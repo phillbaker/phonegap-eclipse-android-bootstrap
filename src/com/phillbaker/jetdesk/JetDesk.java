d97ba8c665144b199470a3059af78fcbe0e99deb
pbaker <me@retrodict.com>
2012-02-26 16:42:12 -0500

    Initial commit

---
+++ src/com/phillbaker/jetdesk/JetDesk.java
@@ -0,0 +1,9 @@
+package com.phillbaker.jetdesk;
+
+import android.app.Activity;
+import android.os.Bundle;
+import com.phonegap.*;
+
+public class JetDesk extends DroidGap {
+
+}
---
+++ src/com/phillbaker/jetdesk/JetdeskActivity.java
@@ -0,0 +1,13 @@
+package com.phillbaker.jetdesk;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class JetdeskActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
