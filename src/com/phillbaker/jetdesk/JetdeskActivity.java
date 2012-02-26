ef99955fb78626ea8b4f1057bb4eeaa6586ff316
pbaker <me@retrodict.com>
2012-02-26 16:43:30 -0500

    Add phonegap initial setup

--- src/com/phillbaker/jetdesk/JetdeskActivity.java
+++ src/com/phillbaker/jetdesk/JetdeskActivity.java
@@ -1,13 +1,13 @@
 package com.phillbaker.jetdesk;
 
-import android.app.Activity;
 import android.os.Bundle;
+import com.phonegap.*;
 
-public class JetdeskActivity extends Activity {
+public class JetdeskActivity extends DroidGap {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        super.loadUrl("file:///android_asset/www/index.html");
     }
 }
